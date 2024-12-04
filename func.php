265a174a7b6aa0a4313e503436732d96e53255b9
EmediongUdo13 <finemimi08@gmail.com>
2024-12-04 14:55:36 +0100

    Update func.php

--- func.php
+++ func.php
@@ -45,7 +45,7 @@ function statem($a, $b, $c){
         } elseif(premid) {
             return "lesser";
         } else{
-            jkjgjfj
+            echo "jkjgjfj"
         }
     } else {
         return "lesser";
